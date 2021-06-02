d6e703060accc779be40c3e4f90749e82c12c8c8
hieule <le009660@stthomas.edu>
2021-06-02 11:26:22 -0500

    add relationships to models. Closes #8.

--- src/main/java/io/hieule/travelyapp/model/travelEntity.java
+++ src/main/java/io/hieule/travelyapp/model/travelEntity.java
@@ -1,25 +1,22 @@
 package io.hieule.travelyapp.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by Hieu Le on 6/2/2021
  */
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class travelEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
