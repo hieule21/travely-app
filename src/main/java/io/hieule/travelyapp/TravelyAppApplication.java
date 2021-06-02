c114e902c08d01bfbe0176ab23bf0cd22e72c40a
hieule <le009660@stthomas.edu>
2021-06-02 10:31:42 -0500

    initialize app. Closes #1.

---
+++ src/main/java/io/hieule/travelyapp/TravelyAppApplication.java
@@ -0,0 +1,13 @@
+package io.hieule.travelyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelyAppApplication.class, args);
+    }
+
+}
